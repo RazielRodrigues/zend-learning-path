38562959183e1479c52ae4fe2f1f637d15367168
Raziel Rodrigues <razielx3@live.com>
2022-06-07 22:11:09 -0300

    first model class

---
+++ app/module/Pessoa/src/Model/Pessoa.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Pessoa\Model;
+
+class Pessoa
+{
+    public $id;
+    public $nome;
+    public $sobrenome;
+    public $email;
+    public $status;
+
+    public function exchangeArray(array $data)
+    {
+        $this->id = !empty($data['id']) ? $data['id'] : null;
+        $this->nome = !empty($data['nome']) ? $data['nome'] : null;
+        $this->sobrenome = !empty($data['sobrenome']) ? $data['sobrenome'] : null;
+        $this->email = !empty($data['email']) ? $data['email'] : null;
+        $this->status = !empty($data['status']) ? $data['status'] : null;
+    }
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sobrenome
+     */ 
+    public function getSobrenome()
+    {
+        return $this->sobrenome;
+    }
+
+    /**
+     * Set the value of sobrenome
+     *
+     * @return  self
+     */ 
+    public function setSobrenome($sobrenome)
+    {
+        $this->sobrenome = $sobrenome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of status
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
